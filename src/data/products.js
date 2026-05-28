bdbf430720e25a5b912a4ec196b40116dfec00c5
Laerte Lima <laerte.mancio@gmail.com>
2026-05-28 15:25:18 -0300

    ajustes links imagens

--- src/data/products.js
+++ src/data/products.js
@@ -1,23 +1,23 @@
 const products = [
   {
     id: 1,
-    title: "Inkjet Industrial X1",
+    title: "Inkjet Industrial TTO",
     description: "Ideal para codificação industrial.",
     image: "https://res.cloudinary.com/deocq6es3/image/upload/v1779978098/8220XL_ptdizx.png"
   },
 
   {
     id: 2,
-    title: "Datador Industrial Z2",
+    title: "Datador Industrial TTO 2",
     description: "Alta performance e baixo custo.",
     image: "https://res.cloudinary.com/deocq6es3/image/upload/v1779978098/32LT_vjztsj.png"
   },
 
   {
     id: 3,
-    title: "Codificador Térmico A5",
+    title: "Codificador Laser",
     description: "Excelente custo-benefício.",
-    image: "https://res.cloudinary.com/deocq6es3/image/upload/v1779978098/53501_vsl9e9.jpg"
+    image: "https://res.cloudinary.com/deocq6es3/image/upload/v1779992637/co2_rd3klz.jpg"
   }
 ]
 
